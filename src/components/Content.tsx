6b2e0c14cc4bf72faa7f10540efa032988b1d6a7
Lucas Lessa <lucaslessa14@hotmail.com>
2021-12-24 16:02:03 -0300

    challenge done!

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,37 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+
+import '../styles/content.scss';
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  gender: string;
+  movies: MovieProps[];
+}
+
+export function Content({ gender, movies } : ContentProps) { 
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> { gender }</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
